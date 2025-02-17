4fbef9546e916d6c974e00cd69fedd0f754226b4
1leozhao <leo.zhao@vanderilt.edu>
2025-02-17 13:42:35 -0600

    scrollable tabs

--- app/(app)/token/[address]/page.tsx
+++ app/(app)/token/[address]/page.tsx
@@ -6,9 +6,11 @@ import Header from "./_components/header";
 import TokenDashboardTabs from "./_components/tabs";
 import SidePanel from "./_components/side-panel";
 import ResizableLayout from "./_components/resizable-layout";
+import { getTokenOverview } from '@/services/birdeye';
 
 const TokenPage = async ({ params }: { params: Promise<{ address: string }> }) => {
     const { address } = await params;
+    const tokenOverview = await getTokenOverview(address);
 
     return (
         <div className="flex flex-col gap-2 h-full max-h-full overflow-hidden">
@@ -17,7 +19,7 @@ const TokenPage = async ({ params }: { params: Promise<{ address: string }> }) =
                 chartComponent={<TokenChart mint={address} />}
                 tabsComponent={
                     <Suspense fallback={<Skeleton className="h-full w-full m-2" />}>
-                        <TokenDashboardTabs address={address} />
+                        <TokenDashboardTabs address={address} tokenOverview={tokenOverview} />
                     </Suspense>
                 }
                 sidePanelComponent={<SidePanel address={address} />}
